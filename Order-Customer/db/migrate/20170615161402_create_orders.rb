2c80e2eee69fe711b574a21fc4d04a2553e4112d
vrunda2712 <angy.v27@gmail.com>
2017-06-15 12:20:26 -0400

    Generated models for Order-Customer and setup associates

---
+++ Order-Customer/db/migrate/20170615161402_create_orders.rb
@@ -0,0 +1,10 @@
+class CreateOrders < ActiveRecord::Migration[5.1]
+  def change
+    create_table :orders do |t|
+      t.integer :order_number
+      t.references :customer, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
